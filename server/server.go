70ee0f23e25660565f572780bff5aa102821a3cd
Radek Ryckowski <radek.ryckowski@gmail.com>
2024-08-07 20:32:09 +0100

    basic client, improved Set
    Signed-off-by: Radek Ryckowski <radek.ryckowski@gmail.com>

--- server/server.go
+++ server/server.go
@@ -2,23 +2,21 @@ package server
 
 import (
 	"context"
-	"log"
-	"net/http"
 	"sync"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/radek-ryckowski/ssdc/cache"
 	pb "github.com/radek-ryckowski/ssdc/proto/cache"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
-	nodeErrors = promauto.NewCounter(prometheus.CounterOpts{
+	nodeErrors = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "node_errors_total",
 		Help: "Total number of connection errors hits",
-	})
+	}, []string{"peer_address"})
 )
 
 type Server struct {
@@ -28,53 +26,55 @@ type Server struct {
 	peers []pb.CacheServiceClient
 }
 
-func (s *Server) Start(httpAddress string) {
+func (s *Server) Start() {
 	go s.c.WaitForSignal()
-
-	// Initialize Prometheus metrics endpoint
-	http.Handle("/metrics", promhttp.Handler())
-	go func() {
-		err := http.ListenAndServe(httpAddress, nil)
-		if err != nil {
-			log.Fatalf("failed to start Prometheus metrics endpoint: %v", err)
-		}
-	}()
 }
 
 func (s *Server) Set(ctx context.Context, req *pb.SetRequest) (*pb.SetResponse, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	// Store the value locally in cache
-	err := s.c.Store([]byte(req.Key), []byte(req.Value))
+	value, err := proto.Marshal(req.Value)
+	nodeCount := int32(0)
 	if err != nil {
 		return &pb.SetResponse{Success: false}, err
+	}
+	// Store the value locally in cache
+	err = s.c.Store([]byte(req.Uuid), value)
+	if err != nil {
+		return &pb.SetResponse{Success: false}, err
+	}
+	if req.Local {
+		nodeCount++
+		return &pb.SetResponse{Success: true, ConsistentNodes: nodeCount}, nil
 	}
 	quorum := len(s.peers) / 2 // local +1
 	// Store the value in the peers using go routines to avoid blocking if quorum number of peers return success response is true
 	// use context with timeout to avoid blocking forever
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+
 	var successCount int
 	var wg sync.WaitGroup
-	wg.Add(len(s.peers))
+	wg.Add(len(s.peers)) // wait only for quorum number of peers to respond with success
 	for _, peer := range s.peers {
 		go func(peer pb.CacheServiceClient) {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
 			defer wg.Done()
-			resp, err := peer.Set(ctx, &pb.SetRequest{Key: req.Key, Value: req.Value})
+			resp, err := peer.Set(ctx, &pb.SetRequest{Uuid: req.Uuid, Value: req.Value, Local: true})
 			if err != nil {
-				nodeErrors.Inc()
+				nodeErrors.WithLabelValues().Inc() //TODO add peer address to the metric as label
 				return
 			}
 			if resp.Success {
 				successCount++
+				nodeCount++
 			}
 		}(peer)
 	}
 	wg.Wait()
 	if successCount < quorum {
-		return &pb.SetResponse{Success: false}, nil
+		return &pb.SetResponse{Success: false, ConsistentNodes: nodeCount}, nil
 	}
-	return &pb.SetResponse{Success: true}, nil
+	return &pb.SetResponse{Success: true, ConsistentNodes: nodeCount}, nil
 }
 
 func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
