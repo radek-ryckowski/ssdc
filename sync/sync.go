7fed2e239bcadd1ff1dcaaed820edea096216484
Radek Ryckowski <radek.ryckowski@gmail.com>
2024-08-27 13:14:53 +0100

    Enabling slog
    Signed-off-by: Radek Ryckowski <radek.ryckowski@gmail.com>

--- sync/sync.go
+++ sync/sync.go
@@ -21,13 +21,14 @@ type Node struct {
 }
 
 type Updater struct {
-	Path         string // Path to updater database
-	db           *lotusdb.DB
-	mx           sync.RWMutex
-	startSyncing chan bool
-	StopTicker   chan bool
-	cacheClients map[int]*cluster.CacheClient
-	GetKeyCall   func(key []byte) ([]byte, error)
+	Path               string // Path to updater database
+	db                 *lotusdb.DB
+	mx                 sync.RWMutex
+	startSyncing       chan bool
+	StopTicker         chan bool
+	cacheClients       map[int]*cluster.CacheClient
+	GetKeyCall         func(key []byte) ([]byte, error)
+	walkAndSendRunning bool
 }
 
 func New(path string) *Updater {
@@ -53,7 +54,7 @@ func (u *Updater) Start() {
 		for range ticker.C {
 			select {
 			case <-u.startSyncing:
-				// Start syncing logic here
+				u.WalkAndSend()
 			case <-u.StopTicker:
 				ticker.Stop()
 				u.db.Close()
@@ -82,6 +83,19 @@ func (u *Updater) UpdatePeer(node *cluster.CacheClient) {
 
 // WalkAndSend method to walk through the database and send the data to the peers
 func (u *Updater) WalkAndSend() {
+	u.mx.Lock()
+	if u.walkAndSendRunning {
+		u.mx.Unlock()
+		return
+	}
+	u.walkAndSendRunning = true
+	u.mx.Unlock()
+	defer func() {
+		u.mx.Lock()
+		u.walkAndSendRunning = false
+		u.mx.Unlock()
+	}()
+
 	// Walk through the database
 	iter, err := u.db.NewIterator(lotusdb.IteratorOptions{Reverse: false})
 	if err != nil {
@@ -123,7 +137,6 @@ func (u *Updater) WalkAndSend() {
 			u.db.Delete(uuid)
 			u.mx.Unlock()
 		}
-
 		iter.Next()
 	}
 
